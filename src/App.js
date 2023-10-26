0bdcf90ef07e425c67bcabeb7520bbf33d93bfdc
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-10-26 21:29:37 +0300

    eager loading

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React, { Suspense, useState } from "react";
 import "./App.css";
 const Component = React.lazy(() => import("./Component"));
 
+function loadComponent() {
+  import("./Component");
+}
+
 function App() {
   const [showComponent, setShowComponent] = useState(false);
 
   return (
     <div>
-      <Suspense fallback={<p>Parent loading...</p>}>
-        <h1 onClick={() => setShowComponent(!showComponent)}>Hello World</h1>
+      <h1
+        onMouseOver={loadComponent}
+        onClick={() => setShowComponent(!showComponent)}
+      >
+        Hello World
+      </h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <p>This is something</p>
         {showComponent && <Component />}
-        <Suspense fallback={<div>Loading...</div>}>
-          <p>This is something</p>
-        </Suspense>
       </Suspense>
     </div>
   );
